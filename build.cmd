a98e7a519be48a61e60558a3afd868982383f0be
Leon Sorokin <leeoniya@gmail.com>
2016-03-14 20:15:40 -0500

    clean up polyfills & add to min + build

--- build.cmd
+++ build.cmd
@@ -1,1 +1,3 @@
 java -jar compiler.jar --language_in ECMASCRIPT5 --js src/domvm.js --js src/util.js --js src/view.js --js src/html.js --js src/watch.js --js src/route.js --js_output_file dist/domvm.min.js
+
+java -jar compiler.jar --jscomp_off=uselessCode --language_in ECMASCRIPT5 --js src/polyfills/polyfills.js --js src/polyfills/polyfill.element.matches.js --js src/polyfills/request-frame.src.js --js src/polyfills/Promise.js --js src/polyfills/fetch.js --js_output_file dist/polyfills.min.js
---
+++ src/polyfills/polyfills.js
@@ -0,0 +1,7 @@
+/**
+* @preserve Contains the following:
+* https://gist.github.com/elijahmanor/6452535
+* https://github.com/julienetie/request-frame
+* https://github.com/taylorhakes/promise-polyfill
+* https://github.com/github/fetch
+*/
