a5fbc9234e0fb984e008c46b8f9ceb6cee8b497f
Leon Sorokin <leeoniya@gmail.com>
2016-12-01 18:20:13 -0600

    ThreaditJS demo

---
+++ demos/ThreaditJS/app.js
@@ -0,0 +1,298 @@
+T.time("Setup");
+
+var el = domvm.defineElement,
+	tx = domvm.defineText,
+	vw = domvm.defineView,
+	curView,
+	rootVm;
+
+Object.defineProperties(Array.prototype, {
+    flatMap: {
+        value: function(lambda) {
+            return Array.prototype.concat.apply([], this.map(lambda));
+        }
+    }
+});
+
+function error(msg, data) {
+//	console.error(msg, data);
+
+	return Promise.reject({
+		message: msg,
+		data: data,
+	});
+}
+
+// api endpoints with data extraction & transport fault handling
+const endPoints = {
+	getThreadList: _ =>
+		xr.get(T.apiUrl  + "/threads/")
+		.then(r => r.data.data)
+		.catch(r => error("Could not load thread list", r)),
+	getComments: parentId =>
+		xr.get(T.apiUrl  + "/comments/" + parentId)
+		.then(r => T.transformResponse(r.data))
+		.catch(r => error("Could not load comments of #" + parentId, r)),
+	postThread: text =>
+		xr.post(T.apiUrl + "/threads/create", {text: text})
+		.catch(r => error("Could not create thread", r)),
+	postComment: (text, parentId) =>
+		xr.post(T.apiUrl + "/comments/create", {text : text, parent: parentId})
+		.catch(r => error("Could not create comment", r)),
+};
+
+function statusTpl(msg) {
+	return el("p.status", {style: "font-weight: bold;"},  msg);
+}
+
+// error formatter
+function errorTpl(error) {
+	var data = error.data;
+
+	try {
+		data.response = JSON.parse(data.response);
+	} catch(e) {}
+
+	return el(".error", [
+		el("strong", error.message),
+		el("hr"),
+		el("pre", JSON.stringify(data, null, 2)),
+	]);
+}
+
+function ThreaditView(vm, deps) {
+	return function() {
+		return el(".body", [
+			el("p.head_links", [
+				el("a", {href: "https://github.com/leeoniya/domvm/tree/master/demos/threaditjs"}, "Source"),
+				tx(" | "),
+				el("a", {href: "http://threaditjs.com"}, "ThreaditJS Home")
+			]),
+			el("h2", [
+				el("a", {href: "http://leeoniya.github.io/domvm/demos/threaditjs/"}, "ThreaditJS: domvm")
+			]),
+			el(".main", [
+				curView
+			])
+		]);
+	};
+}
+
+function ThreadListView(vm) {
+	function onClick(threadId) {
+		loadComments(threadId);
+		return false;
+	}
+
+	return function(vm, data) {
+		return data.error ? errorTpl(data.error)
+		: data.threads.length == 0 ? statusTpl("Loading thread list...")
+		: el(".threads", data.threads.flatMap(thread => [
+			el("p", [
+				//	deps.router.href("thread", {id: thread.id})			r.goto("comment", [5]);  onclick: [onClick, thread.id]
+				el("a", {href: router.href("thread", {id: thread.id}), _raw: true}, T.trimTitle(thread.text))
+			]),
+			el("p.comment_count", thread.comment_count + " comment" + (thread.comment_count !== 1 ? "s" : "")),
+			el("hr"),
+		]).concat(
+			vw(ThreadSubmitView)
+		));
+	};
+}
+
+// status
+const ERROR			= -1,
+	  LOADING		= 0,
+	  INTERACTING	= 1,
+	  SUBMITTING	= 2,
+	  RELOADING		= 3,
+	  LOADED		= 4;
+
+function ThreadSubmitView(vm) {
+	var status = domvm.prop(LOADED, vm.redraw.bind(vm));
+	var error;
+
+	function postThread() {
+		status(SUBMITTING);
+
+		endPoints.postThread(vm.refs.text.el.value)
+			.then(_ => {
+				status(RELOADING);
+				return loadThreadList(true);
+			})
+			.then(_ => {
+				status(LOADED, false);
+				vm.parent().redraw();
+			})
+			.catch(e => {
+				error = e;
+				status(ERROR);
+			});
+
+		return false;
+	}
+
+	return function(vm) {
+		return (
+		  status() == ERROR      ? errorTpl(error)
+		: status() == SUBMITTING ? statusTpl("Submitting thread...")
+		: status() == RELOADING  ? statusTpl("Submitted! Reloading thread list...")
+		: el("form", {onsubmit: postThread}, [
+				el("textarea", {_ref: "text"}),
+				el("input", {type: "submit", value: "Post!"})
+			])
+		)
+	}
+}
+
+// CommentsView
+// sub-view (will be used recursively)
+function ThreadBranchView(vm) {
+	return function(vm, data) {
+		return data.error ? errorTpl(data.error)
+		: data.comment == null ? statusTpl("Loading comments of thread #" + data.threadId)
+		: el(".comment", [
+			el("p", {_raw: true}, data.comment.text),
+			vw(CommentReplyView, data.comment, false),
+			el(".children", data.comment.children.map(comment2 =>
+				vw(ThreadBranchView, {comment: comment2}, false)
+			))
+		]);
+	}
+}
+
+// todo: should optionally set route?
+function setView(view, data) {
+	curView = vw(view, data, false);
+	rootVm && rootVm.redraw();
+}
+
+function loadEndpt(endPt, reqArgs, view, preData, procData) {
+	// sets loading message
+	if (preData != null)
+		setView(view, preData);
+
+	return endPt.apply(null, reqArgs)
+		.then(r => setView(view, procData(r)))
+		.catch(e => setView(view, {error: e}))
+}
+
+function loadThreadList(soft) {
+	return loadEndpt(endPoints.getThreadList, [], ThreadListView, soft ? null : {threads: []}, l => ({threads: l}));
+}
+
+function loadComments(threadId, soft) {
+	return loadEndpt(endPoints.getComments, [threadId], ThreadBranchView, soft ? null : {comment: null, threadId: threadId}, c => ({comment: c.root, threadId: threadId}));
+}
+
+
+// sub-sub view
+function CommentReplyView(vm, comment) {
+	var redraw = vm.redraw.bind(vm);
+	var status = domvm.prop(LOADED, redraw);
+	var error;
+
+	var tmpComment = domvm.prop("", redraw);
+
+	function toggleReplyMode(e, node) {
+		status(INTERACTING);
+		return false;
+	}
+
+	function postComment(e, node) {
+		status(SUBMITTING);
+
+		// TODO: flatten? dry?
+		endPoints.postComment(tmpComment(), comment.id)
+			.then(comment2 => {
+				tmpComment("", false);
+				status(RELOADING);
+				return endPoints.getComments(comment.id);
+			})
+			.then(c => {
+				comment.children = Object.values(c.lookup).slice(1);	// have to slice off self
+				status(LOADED, false);
+				vm.parent().redraw();
+			})
+			.catch(e => {
+				error = e;
+				status(ERROR);
+			});
+
+		return false;
+	}
+
+	function previewReply(e, node) {
+		tmpComment(node.el.value);
+	}
+
+	return function() {
+		return (
+		  status() == ERROR			? errorTpl(error)
+		: status() == SUBMITTING	? statusTpl("Submitting comment...")
+		: status() == RELOADING		? statusTpl("Submitted! Reloading comments...")
+		: el(".reply", [
+			status() == INTERACTING ?
+			el("form", {onsubmit: postComment}, [
+				el("textarea", {
+					value: tmpComment(),
+					onkeyup: previewReply,
+				}),
+				el("input", {type: "submit", value: "Reply!"}),
+				el(".preview", {_raw: true}, T.previewComment(tmpComment())),
+			])
+			: el("a", {href: "#", onclick: toggleReplyMode}, "Reply!")
+			])
+		);
+	}
+}
+
+// init app, init view, init router, refresh router()
+
+var router = domvm.createRouter(ThreaditRouter);
+
+var hooks = {
+	willRedraw: vm => console.time("redraw vm #" + vm.id),
+	didRedraw: vm => console.timeEnd("redraw vm #" + vm.id),
+};
+
+var deps = {app: null, router: router};
+
+rootVm = domvm.createView(ThreaditView, deps, false, {hooks: hooks}).mount(document.body);
+
+router.refresh();
+
+function ThreaditRouter(router) {
+	/*
+	router.config({
+		useHist: false,
+	//	root: "/domvm/demos/threaditjs",
+	});
+	*/
+
+	var titlePre = "ThreaditjS: domvm | ";
+
+	return {
+		threadList: {
+			path: "/",
+			onenter: function(segs) {
+				T.timeEnd("Setup");
+				document.title = titlePre + "Thread List";
+				loadThreadList();
+			},
+		},
+		thread: {
+			path: "/threads/:id",
+			vars: {id: /[a-zA-Z0-9]{5,7}/},
+			onenter: function(segs) {
+				T.timeEnd("Setup");
+				document.title = titlePre + "Thread #" + segs.id;
+				loadComments(segs.id);
+			},
+		},
+//		_noMatch: {
+//			path: "/404",
+//		}
+	};
+}
+
