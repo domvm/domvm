c3c91e7c6d392233b1c7c24801f2415e056f3076
e1ectronic <e1@yandex.ru>
2017-05-12 23:58:12 +0300

    Added Object.create polyfill to ie8.js

--- dist/ie8.js
+++ dist/ie8.js
@@ -188,3 +188,17 @@ if (!Element.prototype.matches) {
         return false;
       }
 }
+
+if (typeof Object.create !== 'function') {
+  Object.create = function(o, props) {
+    function F() {}
+    F.prototype = o;
+
+    if (typeof(props) === "object")
+      for (var prop in props)
+        if (props.hasOwnProperty((prop)))
+          F[prop] = props[prop];
+
+    return new F();
+  }
+}
