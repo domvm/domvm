36b1a9d1c8e5959638cc4bfb31847759d4646e63
Leon Sorokin <leeoniya@gmail.com>
2017-05-19 00:32:23 -0500

    split up preProc a bit

--- src/view/preProc.js
+++ src/view/preProc.js
@@ -30,40 +30,42 @@ export function preProc(vnew, parent, idx, ownVm) {
 	if (vnew.hooks && vnew.hooks.willRemove || ownVm && ownVm.hooks && ownVm.hooks.willUnmount)
 		setDeepRemove(vnew);
 
-	if (isArr(vnew.body)) {
-		// declarative elems, comments, text nodes
-		var body = vnew.body;
+	if (isArr(vnew.body))
+		preProcBody(vnew);
+	else if (isStream(vnew.body))
+		vnew.body = hookStream(vnew.body, getVm(vnew));
+}
 
-		for (var i = 0; i < body.length; i++) {
-			var node2 = body[i];
+export function preProcBody(vnew) {
+	var body = vnew.body;
 
-			// remove false/null/undefined
-			if (node2 === false || node2 == null)
-				body.splice(i--, 1);
-			// flatten arrays
-			else if (isArr(node2))
-				insertArr(body, node2, i--, 1);
-			else {
-				if (node2.type == null)
-					body[i] = node2 = defineText(""+node2);
+	for (var i = 0; i < body.length; i++) {
+		var node2 = body[i];
 
-				if (node2.type === TEXT) {
-					// remove empty text nodes
-					if (node2.body == null || node2.body === "")
-						body.splice(i--, 1);
-					// merge with previous text node
-					else if (i > 0 && body[i-1].type === TEXT) {
-						body[i-1].body += node2.body;
-						body.splice(i--, 1);
-					}
-					else
-						preProc(node2, vnew, i, null);
+		// remove false/null/undefined
+		if (node2 === false || node2 == null)
+			body.splice(i--, 1);
+		// flatten arrays
+		else if (isArr(node2))
+			insertArr(body, node2, i--, 1);
+		else {
+			if (node2.type == null)
+				body[i] = node2 = defineText(""+node2);
+
+			if (node2.type === TEXT) {
+				// remove empty text nodes
+				if (node2.body == null || node2.body === "")
+					body.splice(i--, 1);
+				// merge with previous text node
+				else if (i > 0 && body[i-1].type === TEXT) {
+					body[i-1].body += node2.body;
+					body.splice(i--, 1);
 				}
 				else
 					preProc(node2, vnew, i, null);
 			}
+			else
+				preProc(node2, vnew, i, null);
 		}
 	}
-	else if (isStream(vnew.body))
-		vnew.body = hookStream(vnew.body, getVm(vnew));
 }
