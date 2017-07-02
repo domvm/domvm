a05f8fbcd98b6daaea047f52868fb938ceac24b9
Leon Sorokin <leeoniya@gmail.com>
2017-07-02 17:47:03 -0500

    additionally autokey radio inputs by [value] since groups share [name]

--- dist/micro/domvm.micro.js
+++ dist/micro/domvm.micro.js
@@ -388,7 +388,7 @@ function initElementNode(tag, attrs, body, flags) {
 			else if (isSet(mergedAttrs.id))
 				{ node.key = mergedAttrs.id; }
 			else if (isSet(mergedAttrs.name))
-				{ node.key = mergedAttrs.name; }
+				{ node.key = mergedAttrs.name + (mergedAttrs.type == "radio" ? mergedAttrs.value : ""); }
 		}
 	}
 
