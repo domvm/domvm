a83783157029f3b266a703cb73e58a2a4701abe9
Leon Sorokin <leeoniya@gmail.com>
2017-07-02 09:19:53 -0500

    3.0.0

--- dist/client/domvm.client.js
+++ dist/client/domvm.client.js
@@ -4,7 +4,7 @@
 *
 * domvm.full.js - DOM ViewModel
 * A thin, fast, dependency-free vdom view layer
-* @preserve https://github.com/leeoniya/domvm (3.x-dev, client)
+* @preserve https://github.com/leeoniya/domvm (v3.0.0, client)
 */
 
 (function (global, factory) {
--- dist/full/domvm.full.js
+++ dist/full/domvm.full.js
@@ -4,7 +4,7 @@
 *
 * domvm.full.js - DOM ViewModel
 * A thin, fast, dependency-free vdom view layer
-* @preserve https://github.com/leeoniya/domvm (3.x-dev, full)
+* @preserve https://github.com/leeoniya/domvm (v3.0.0, full)
 */
 
 (function (global, factory) {
