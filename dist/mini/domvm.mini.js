2eefd1d5bb3cf4aa739f9afd44dfa3df1c690bb3
Leon Sorokin <leeoniya@gmail.com>
2017-07-03 15:44:24 -0500

    3.0.1

--- dist/mini/domvm.mini.js
+++ dist/mini/domvm.mini.js
@@ -4,7 +4,7 @@
 *
 * domvm.full.js - DOM ViewModel
 * A thin, fast, dependency-free vdom view layer
-* @preserve https://github.com/leeoniya/domvm (v3.0.0, mini)
+* @preserve https://github.com/leeoniya/domvm (v3.0.1, mini)
 */
 
 (function (global, factory) {
