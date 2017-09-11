9519b083d83dce9f5f237bfd93b2906f61f09526
Leon Sorokin <leeoniya@gmail.com>
2017-09-11 12:28:51 -0500

    [tests] shim for Function#name (IE11)

--- test/init.js
+++ test/init.js
@@ -34,3 +34,16 @@ function anonView(tpl) {
 		};
 	}
 }
+
+// Fix Function#name on browsers that do not support it (IE):
+if (!(function f() {}).name) {
+	Object.defineProperty(Function.prototype, 'name', {
+		get: function() {
+			var name = (this.toString().match(/^function\s*([^\s(]+)/) || [])[1];
+			// For better performance only parse once, and then cache the
+			// result through a new accessor for repeated access.
+			Object.defineProperty(this, 'name', { value: name });
+			return name;
+		}
+	});
+}
