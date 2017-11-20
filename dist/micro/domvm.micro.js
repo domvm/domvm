b15563c894a3fcc4aa81eb9f71c21060c879bd65
Leon Sorokin <leeoniya@gmail.com>
2017-11-19 21:51:11 -0600

    remove build script hack which prevented inlining of syncChildren by Closure looks like Chrome fixed its deopt bug

--- dist/micro/domvm.micro.js
+++ dist/micro/domvm.micro.js
@@ -1712,9 +1712,6 @@ function lazyList(items, cfg) {
 	return self;
 }
 
-// prevent GCC from inlining some large funcs (which negatively affects Chrome's JIT)
-window.syncChildren = syncChildren;
-
 var nano = {
 	config: config,
 
--- dist/nano/domvm.nano.js
+++ dist/nano/domvm.nano.js
@@ -1631,9 +1631,6 @@ function lazyList(items, cfg) {
 	return self;
 }
 
-// prevent GCC from inlining some large funcs (which negatively affects Chrome's JIT)
-window.syncChildren = syncChildren;
-
 var nano$1 = {
 	config: config,
 
