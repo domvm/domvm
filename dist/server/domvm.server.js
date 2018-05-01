f47bba21a25e4b76451d4e56345f28e28f5cb3b4
Leon Sorokin <leeoniya@gmail.com>
2018-05-01 14:39:29 -0500

    3.3.0

--- dist/server/domvm.server.js
+++ dist/server/domvm.server.js
@@ -4,7 +4,7 @@
 *
 * domvm.js (DOM ViewModel)
 * A thin, fast, dependency-free vdom view layer
-* @preserve https://github.com/leeoniya/domvm (v3.2.8, server build)
+* @preserve https://github.com/leeoniya/domvm (v3.3.0, server build)
 */
 
 (function (global, factory) {
