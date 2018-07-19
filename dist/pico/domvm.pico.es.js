efdb49e919bff4b875d9630ad47743cb6bdf8bd0
Leon Sorokin <leeoniya@gmail.com>
2018-07-19 01:25:52 -0500

    3.4.2

--- dist/pico/domvm.pico.es.js
+++ dist/pico/domvm.pico.es.js
@@ -4,7 +4,7 @@
 *
 * domvm.js (DOM ViewModel)
 * A thin, fast, dependency-free vdom view layer
-* @preserve https://github.com/domvm/domvm (v3.4.1, pico build)
+* @preserve https://github.com/domvm/domvm (v3.4.2, pico build)
 */
 
 // NOTE: if adding a new *VNode* type, make it < COMMENT and renumber rest.
