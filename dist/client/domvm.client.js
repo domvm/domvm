7d0b5870d074467da81e8c16a324a98667c7c2b4
Leon Sorokin <leeoniya@gmail.com>
2018-10-01 11:33:37 -0500

    3.4.6

--- dist/client/domvm.client.js
+++ dist/client/domvm.client.js
@@ -4,7 +4,7 @@
 *
 * domvm.js (DOM ViewModel)
 * A thin, fast, dependency-free vdom view layer
-* @preserve https://github.com/domvm/domvm (v3.4.5, client build)
+* @preserve https://github.com/domvm/domvm (v3.4.6, client build)
 */
 
 (function (global, factory) {
--- dist/nano/domvm.nano.js
+++ dist/nano/domvm.nano.js
@@ -4,7 +4,7 @@
 *
 * domvm.js (DOM ViewModel)
 * A thin, fast, dependency-free vdom view layer
-* @preserve https://github.com/domvm/domvm (v3.4.5, nano build)
+* @preserve https://github.com/domvm/domvm (v3.4.6, nano build)
 */
 
 (function (global, factory) {
