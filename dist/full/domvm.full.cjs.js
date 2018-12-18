b6ce798c76d13a80098b6edb5029426795166093
Leon Sorokin <leeoniya@gmail.com>
2018-12-18 11:10:17 -0600

    3.4.7

--- dist/full/domvm.full.cjs.js
+++ dist/full/domvm.full.cjs.js
@@ -4,7 +4,7 @@
 *
 * domvm.js (DOM ViewModel)
 * A thin, fast, dependency-free vdom view layer
-* @preserve https://github.com/domvm/domvm (v3.4.7-dev, full build)
+* @preserve https://github.com/domvm/domvm (v3.4.7, full build)
 */
 
 'use strict';
