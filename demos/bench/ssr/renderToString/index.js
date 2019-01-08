7606f8ebfd73efdd04d86bae01aad2d1a01ae780
Leon Sorokin <leeoniya@gmail.com>
2019-01-08 16:57:24 -0600

    ssr bench deps update

--- demos/bench/ssr/renderToString/index.js
+++ demos/bench/ssr/renderToString/index.js
@@ -3,8 +3,8 @@
 const CHILDREN_NUM = 200;
 
 module.exports = {
-	'vidom'   : require('./vidom')(CHILDREN_NUM),
-	'inferno' : require('./inferno')(CHILDREN_NUM),
+//	'vidom'   : require('./vidom')(CHILDREN_NUM),
+//	'inferno' : require('./inferno')(CHILDREN_NUM),
 	'domvm'   : require('./domvm')(CHILDREN_NUM),
 	'preact'  : require('./preact')(CHILDREN_NUM),
 	'react'   : require('./react')(CHILDREN_NUM),
