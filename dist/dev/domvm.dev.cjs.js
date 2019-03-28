a2800fb46bc0d44cd8395924341e20fb536172b5
Leon Sorokin <leeoniya@gmail.com>
2019-03-28 17:25:33 -0500

    ver bump

--- dist/dev/domvm.dev.cjs.js
+++ dist/dev/domvm.dev.cjs.js
@@ -4,7 +4,7 @@
 *
 * domvm.js (DOM ViewModel)
 * A thin, fast, dependency-free vdom view layer
-* @preserve https://github.com/domvm/domvm (v3.4.10, dev build)
+* @preserve https://github.com/domvm/domvm (v3.4.11-dev, dev build)
 */
 
 'use strict';
