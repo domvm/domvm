21ccecc4ed463bd9d82ab880d403ceeabca9ac5b
Lawrence Dol <lawrence@dolhub.com>
2021-11-17 20:14:08 -0800

    Revert to using add/remove listener.

--- src/view/patchEvent.js
+++ src/view/patchEvent.js
@@ -6,50 +6,50 @@ import { devNotify } from "./addons/devmode";
 function exec(fn, args, e, node) {
     let vm = getVm(node),
         out1 = fn.apply(e.currentTarget, args.concat(e, node, vm, vm.data)), // this == currentTarget, NOT vm, to match normal handler
-		out2,
-		out3;
+        out2,
+        out3;
 
-	if (FEAT_ONEVENT) {
+    if (FEAT_ONEVENT) {
         out2 = vm.onevent(e, node, vm, vm.data, args),
         out3 = onevent.call(null,e, node, vm, vm.data, args);
-	}
+    }
 
-	if (out1 === false || out2 === false || out3 === false) {
-		e.preventDefault();
-		return false;
-	}
+    if (out1 === false || out2 === false || out3 === false) {
+    e.preventDefault();
+    return false;
+    }
 }
 
 function handle(e) {
-	let node = e.currentTarget._node;
+    let node = e.currentTarget._node;
 
-	if (node == null)
+    if (node == null)
         return;
 
     let dfn = node.attrs["on" + e.type];
 
-	if (isArr(dfn))
-		exec(dfn[0], dfn.slice(1), e, node);
+    if (isArr(dfn))
+        exec(dfn[0], dfn.slice(1), e, node);
     else
-		exec(dfn,    emptyArr,     e, node);
+        exec(dfn,    emptyArr,     e, node);
 }
 
 export function patchEvent(node, name, nval, oval) {
-	if (nval == oval)
-		return;
+    if (nval == oval)
+        return;
 
-	if (_DEVMODE) {
-		if (nval != null && !isFunc(nval) && !isArr(nval))
-			devNotify("INVALID_HANDLER", [node, nval]);
+    if (_DEVMODE) {
+        if (nval != null && !isFunc(nval) && !isArr(nval))
+            devNotify("INVALID_HANDLER", [node, nval]);
 
-		if (isFunc(nval) && nval.name == '')
-			devNotify("INLINE_HANDLER", [node, nval]);
-	}
+        if (isFunc(nval) && nval.name == '')
+            devNotify("INLINE_HANDLER", [node, nval]);
+    }
 
-	let el = node.el;
+    let el = node.el;
 
-	if (nval == null)
-		el[name] = null;
-	else if (oval == null)
-		el[name] = handle;
+    if (nval == null)
+        el.removeEventListener(name.slice(2),handle);
+    else if (oval == null)
+        el.addEventListener(name.slice(2),handle);
 }
