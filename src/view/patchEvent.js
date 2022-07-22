domvm/domvm

commit 365bafd26699770f0e7ea680baf580394dc872b6

Set event handler `this` to `e.currentTarget`.

src/view/patchEvent.js
@@ -7,7 +7,7 @@ function exec(fn, args, e, node) {
     let vm = getVm(node),
 		dvmargs = [e, node, vm, vm.data],
 		evtargs,
-		out1 = fn.apply(vm, args.concat(dvmargs)),
+        out1 = fn.apply(e.currentTarget, args.concat(e, node, vm, vm.data)), // this == currentTarget, NOT vm, to match normal handler
 		out2,
 		out3;
 

commit 238b5fa42153fab97bbb2630841fba1a736c5d1e

Critical fix for patchEvent bug which occurs when the view data is an array, introduced in `365bafd...`.
Using an array for the DOMVM args in args.concat was important because concat flattens the first level of arrays.
When the view data is an array, the broken code was flattening its elements into additional arguments instead of
passing the data as an array.

src/view/patchEvent.js
@@ -4,43 +4,45 @@ import { onevent } from './config';
 import { devNotify } from "./addons/devmode";
 
 function handle(evt) {
-    let elm = evt.currentTarget,
-        dfn = elm._node.attrs["on" + evt.type];
+	let elm = evt.currentTarget,
+		dfn = elm._node.attrs["on" + evt.type];
 
-    if (isArr(dfn)) {
-        let fn = dfn[0], args = dfn.slice(1);
-    	let node = getVnode(evt.target),
-            vm = getVm(node),
-    		out1 = fn.apply(evt.currentTarget, args.concat(evt, node, vm, vm.data)), out2, out3;
+	if (isArr(dfn)) {
+		let fn = dfn[0], args = dfn.slice(1);
+		let node = getVnode(evt.target),
+			vm = getVm(node),
+			out1 = fn.apply(elm, args.concat([evt, node, vm, vm.data])), // use of array is necessary due concat flattening first level
+			out2,
+			out3;
 
-        if (FEAT_ONEVENT) {
-    		out2 = vm.onevent(evt, node, vm, vm.data, args),
-    		out3 =    onevent(evt, node, vm, vm.data, args);
-        }
+		if (FEAT_ONEVENT) {
+			out2 = vm.onevent(evt, node, vm, vm.data, args),
+			out3 =    onevent(evt, node, vm, vm.data, args);
+		}
 
-        if (out1 === false || out2 === false || out3 === false)
-    		evt.preventDefault();
-    }
-    else
-        dfn.call(elm, evt);
+		if (out1 === false || out2 === false || out3 === false)
+			evt.preventDefault();
+	}
+	else
+		dfn.call(elm, evt);
 }
 
 export function patchEvent(node, name, nval, oval) {
-    if (nval == oval)
-        return;
+	if (nval == oval)
+		return;
 
-    if (_DEVMODE) {
-        if (nval != null && !isFunc(nval) && !isArr(nval))
-            devNotify("INVALID_HANDLER", [node, nval]);
+	if (_DEVMODE) {
+		if (nval != null && !isFunc(nval) && !isArr(nval))
+			devNotify("INVALID_HANDLER", [node, nval]);
 
-        if (isFunc(nval) && nval.name == '')
-            devNotify("INLINE_HANDLER", [node, nval]);
-    }
+		if (isFunc(nval) && nval.name == '')
+			devNotify("INLINE_HANDLER", [node, nval]);
+	}
 
-    let el = node.el;
+	let el = node.el;
 
-    if (nval == null)
-        el.removeEventListener(name.slice(2), handle);
-    else if (oval == null)
-        el.addEventListener(name.slice(2), handle);
+	if (nval == null)
+		el.removeEventListener(name.slice(2), handle);
+	else if (oval == null)
+		el.addEventListener(name.slice(2), handle);
 }